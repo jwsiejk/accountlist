2726a39850e3410ab57f3ba4417c50eae665d520
jwsiejk <jwsiejk@gmail.com>
2026-01-19 21:51:08 -0500

    Fix worker-loader typings

--- ui/partner-hub/types/worker-loader.d.ts
+++ ui/partner-hub/types/worker-loader.d.ts
@@ -1,3 +1,10 @@
+declare module "*.worker" {
+  const WorkerFactory: {
+    new (): globalThis.Worker;
+  };
+  export default WorkerFactory;
+}
+
 declare module "*.worker.ts" {
   const WorkerFactory: {
     new (): globalThis.Worker;
